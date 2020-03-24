31164f27f703ab2d30563e2d2b12eb893fb2b0eb
Yifan Yang <1195767760@qq.com>
2020-03-24 19:52:54 +0800

    stricter recognition

--- detection.py
+++ detection.py
@@ -65,8 +65,9 @@ while True:
 
                 # Or instead, use the known face with the smallest distance to the new face
                 face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                # print(face_distances)
                 best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
+                if face_distances[best_match_index] < 0.4 and matches[best_match_index]:
                    name = known_face_names[best_match_index]
 
                 face_names.append(name)
@@ -74,33 +75,37 @@ while True:
                     # Face recognized, Do something to open the door:
                     print('Hello, ' + name + '. Welcome home!')
                     pass
+                else:
+                    print('Sorry, you are not welcomed. Please contact the host.')
+                    # print(name)
+                    pass
 
     process_this_frame = not process_this_frame
 
 
-    # # Display the results
-    # for (top, right, bottom, left), name in zip(face_locations, face_names):
-    #     # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-    #     top *= 4
-    #     right *= 4
-    #     bottom *= 4
-    #     left *= 4
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
 
-    #     # Draw a box around the face
-    #     cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-    #     # Draw a label with a name below the face
-    #     cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-    #     font = cv2.FONT_HERSHEY_DUPLEX
-    #     cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    # # Display the resulting image
-    # cv2.imshow('Video', frame)
+    # Display the resulting image
+    cv2.imshow('Video', frame)
 
-    # # Hit 'q' on the keyboard to quit!
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     # cv2.imwrite("recog.jpg", frame)
-    #     break
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # cv2.imwrite("recog.jpg", frame)
+        break
 
 # Release handle to the webcam
 video_capture.release()
