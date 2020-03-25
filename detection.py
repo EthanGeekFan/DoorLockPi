ec85964657debb36ddf2d64c2d25786c4b2be7ef
Yifan Yang <1195767760@qq.com>
2020-03-25 08:43:18 +0800

    update

--- detection.py
+++ detection.py
@@ -10,14 +10,6 @@ import os
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
-# obama_image = face_recognition.load_image_file("me.jpg")
-# obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-
-# Load a second sample picture and learn how to recognize it.
-# biden_image = face_recognition.load_image_file("biden.jpg")
-# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-
 # Load known people from the database/ folder as database
 known_faces = os.listdir('database')
 root = os.path.realpath('database')
